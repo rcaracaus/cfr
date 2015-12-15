3293314291eb0a54a985a2de94aaf9da8fb3c4d6
Robert Caracaus <rcaracaus@gmail.com>
2015-12-14 19:02:19 -0600

    Banner and body column layout

--- js/main.js
+++ js/main.js
@@ -22,3 +22,26 @@ $(".main-menu__link--has-children").click(function(){
   $(this).next(".main-menu--level-2").toggleClass("main-menu--level-2--visible");
   $(this).toggleClass("main-menu__link--active");
 });
+
+
+
+
+
+(function($){
+
+  $('#getting-started').countdown('2015/01/01', function(event) {
+    $(this).html(event.strftime('%w weeks %d days %H:%M:%S'));
+  });
+
+  $('.banner__time').countdown('2016/01/29').on('update.countdown', function(event) {
+      var $this = $(this).html(event.strftime(''
+       + '<span>%-m</span>M '
+       + '<span>%-w</span>W '
+       + '<span>%-d</span>D '
+       + '<span>%H</span>H '
+       + '<span>%M</span>M '
+       + '<span>%S</span>S'));
+  });
+
+})(jQuery);
+
